3571298a91ae224b8198ba7ea3eea8d4771a71e3
William Ravel <wravel@gmail.com>
2021-02-05 20:37:58 +0100

    Create bootcamp api routes

--- server.js
+++ server.js
@@ -3,8 +3,30 @@ const dotenv = require('dotenv');
 
 dotenv.config({ path: './config/config.env' });
 
-const app = express();
 const PORT = process.env.PORT || 5000;
+
+const app = express();
+app.get('/api/v1/bootcamps', (req, res) => {
+  res.status(200).json({ success: true, msg: 'Show all bootcamps' });
+});
+
+app.get('/api/v1/bootcamps/:id', (req, res) => {
+  res.status(200).json({ success: true, msg: `Get bootcamp ${req.params.id}` });
+});
+
+app.post('/api/v1/bootcamps', (req, res) => {
+  res.status(200).json({ success: true, msg: 'Create new bootcamp' });
+});
+
+app.put('/api/v1/bootcamps/:id', (req, res) => {
+  res
+    .status(200)
+    .json({ success: true, msg: `Delete bootcamp ${req.params.id}` });
+});
+
+app.delete('/api/v1/bootcamps/:id', (req, res) => {
+  res.status(200).json({ success: true, msg: `Get bootcamp ${req.params.id}` });
+});
 
 app.listen(PORT, () => {
   console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`);

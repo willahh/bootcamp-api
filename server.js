3ea7c17557bde29b324370e697ca4f39b45e6ce5
William Ravel <wravel@gmail.com>
2021-02-05 20:43:36 +0100

    Add express router

--- server.js
+++ server.js
@@ -1,32 +1,15 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const bootcamps = require('./routes/bootcamps');
 
 dotenv.config({ path: './config/config.env' });
 
 const PORT = process.env.PORT || 5000;
 
 const app = express();
-app.get('/api/v1/bootcamps', (req, res) => {
-  res.status(200).json({ success: true, msg: 'Show all bootcamps' });
-});
 
-app.get('/api/v1/bootcamps/:id', (req, res) => {
-  res.status(200).json({ success: true, msg: `Get bootcamp ${req.params.id}` });
-});
-
-app.post('/api/v1/bootcamps', (req, res) => {
-  res.status(200).json({ success: true, msg: 'Create new bootcamp' });
-});
-
-app.put('/api/v1/bootcamps/:id', (req, res) => {
-  res
-    .status(200)
-    .json({ success: true, msg: `Delete bootcamp ${req.params.id}` });
-});
-
-app.delete('/api/v1/bootcamps/:id', (req, res) => {
-  res.status(200).json({ success: true, msg: `Get bootcamp ${req.params.id}` });
-});
+// Mount routers
+app.use('/api/v1/bootcamps', bootcamps)
 
 app.listen(PORT, () => {
   console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`);

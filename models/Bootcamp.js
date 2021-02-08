03aae2c752ffae1542fb670fd41cb1dedcdb6a34
William Ravel <wravel@gmail.com>
2021-02-08 21:42:04 +0100

    Add suglify middleware

--- models/Bootcamp.js
+++ models/Bootcamp.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const slugify = require('slugify');
 
 const BootcampSchema = new mongoose.Schema({
   name: {
@@ -18,40 +19,40 @@ const BootcampSchema = new mongoose.Schema({
     type: String,
     match: [
       /^(ftp|http|https):\/\/[^ "]+$/,
-      'Please use a valid URL with HTTP or HTTPS'
-    ]
+      'Please use a valid URL with HTTP or HTTPS',
+    ],
   },
   phone: {
     type: String,
-    maxlength: [20, 'Phone number can not be longer than 20 characters']
+    maxlength: [20, 'Phone number can not be longer than 20 characters'],
   },
   email: {
     type: String,
     match: [
       /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
-      'Please add a valid email'
-    ]
+      'Please add a valid email',
+    ],
   },
   adresse: {
     type: String,
-    required: [false, 'Please add an adress']
+    required: [false, 'Please add an adress'],
   },
   location: {
     // GeoJSON Point
     type: {
       type: String,
-      enum: ['Point']
+      enum: ['Point'],
     },
     coordinates: {
       type: [Number],
-      index: '2dsphere'
+      index: '2dsphere',
     },
     formattedAddress: String,
     street: String,
     city: String,
     state: String,
     zipcode: String,
-    country: String
+    country: String,
   },
   careers: {
     type: [String],
@@ -62,39 +63,46 @@ const BootcampSchema = new mongoose.Schema({
       'UI/UX',
       'Data Science',
       'Business',
-      'Other'
-    ]
+      'Other',
+    ],
   },
   averageRating: {
     type: Number,
     min: [1, 'Rating must be at least 1'],
-    max: [10, 'Rating must can not be more than 10']
+    max: [10, 'Rating must can not be more than 10'],
   },
   averageCost: Number,
   photo: {
     type: String,
-    default: 'no-photo.jpg'
+    default: 'no-photo.jpg',
   },
   housing: {
     type: Boolean,
-    default: false
+    default: false,
   },
   jobAssistance: {
     type: Boolean,
-    default: false
+    default: false,
   },
   jobGuarantee: {
     type: Boolean,
-    default: false
+    default: false,
   },
   acceptGi: {
     type: Boolean,
-    default: false
+    default: false,
   },
   createdAt: {
     type: Date,
-    default: Date.now
-  }
+    default: Date.now,
+  },
+});
+
+// Create bootcamp slug from the name
+BootcampSchema.pre('save', function (next) {
+  console.log('Slugify ran', this.name);
+  this.slug = slugify(this.name, { lower: true });
+  next();
 });
 
 module.exports = mongoose.model('Bootcamp', BootcampSchema);
